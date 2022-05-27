b770f48b3b3b7e5cf389641db134cbe5897229bf
Li-HONGYAO <lihy_online@163.com>
2022-05-27 18:01:49 +0800

    updated

--- codes/hello-nest/src/configs/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: Lee
- * @Date: 2022-05-09 15:10:11
- * @LastEditors: Lee
- * @LastEditTime: 2022-05-09 15:10:16
- * @Description:
- */
-export * from './config.interface';
-export * from './configuration';
